807e8c5a42671dc6c1b2371bf7347503f26239b3
Daniel Brice <dbrice@cj.com>
2017-01-04 14:58:54 -0800

    changed whitespace

--- src/main/scala/com/cj/serialization/thrift/package.scala
+++ src/main/scala/com/cj/serialization/thrift/package.scala
@@ -4,24 +4,24 @@ import com.twitter.scrooge.ThriftStruct
 
 package object thrift {
 
- implicit object SerializableThriftStruct extends Serializable[ThriftStruct] {
-   def serialize(t: ThriftStruct): Array[Byte] = {
-     ???
-     Array()
-   }
- }
+  implicit object SerializableThriftStruct extends Serializable[ThriftStruct] {
+    def serialize(t: ThriftStruct): Array[Byte] = {
+      ???
+      Array()
+    }
+  }
 
- class ThriftDeserializable[T <: ThriftStruct] extends Deserializable[T] {
-   def deserialize(bytes: Array[Byte]): Option[T] = {
-     ???
-     None
-   }
- }
+  class ThriftDeserializable[T <: ThriftStruct] extends Deserializable[T] {
+    def deserialize(bytes: Array[Byte]): Option[T] = {
+      ???
+      None
+    }
+  }
 
- def makeThriftDeserializer[T <: ThriftStruct]: Array[Byte] => Option[T] = {
+  def makeThriftDeserializer[T <: ThriftStruct]: Array[Byte] => Option[T] = {
 
-   implicit object DeserializableStruct extends ThriftDeserializable[T]
+    implicit object DeserializableStruct extends ThriftDeserializable[T]
 
-   bytes => deserialize(bytes)
- }
+    bytes => deserialize(bytes)
+  }
 }
--- src/test/scala/ThriftTest.scala
+++ src/test/scala/ThriftTest.scala
@@ -2,88 +2,88 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ThriftTest extends FlatSpec with Matchers {
 
- import com.cj.serialization._
- import com.cj.serialization.thrift._
- import local.test.serialization.thrift.scala.TestRecord
+  import com.cj.serialization._
+  import com.cj.serialization.thrift._
+  import local.test.serialization.thrift.scala.TestRecord
 
- behavior of "SerializableThriftStruct"
+  behavior of "SerializableThriftStruct"
 
- it should "be accessible for TestRecord" in {
-   // given: a `TestRecord`
-   val record = TestRecord("", 0l)
+  it should "be accessible for TestRecord" in {
+    // given: a `TestRecord`
+    val record = TestRecord("", 0l)
 
-   // when: we serialize it
-   serialize(record)
+    // when: we serialize it
+    serialize(record)
 
-   // then: we should make it to the end
-   true
- }
+    // then: we should make it to the end
+    true
+  }
 
- it should "be reusable" in {
-   // given: a bunch of `TestRecord`s
-   val records = for {
-     s <- 'a'.to('z').map(_.toString)
-     n <- 0l.to(9l)
-   } yield TestRecord(s, n)
+  it should "be reusable" in {
+    // given: a bunch of `TestRecord`s
+    val records = for {
+      s <- 'a'.to('z').map(_.toString)
+      n <- 0l.to(9l)
+    } yield TestRecord(s, n)
 
-   // when: we serialize them
-   records.map(serialize[TestRecord])
+    // when: we serialize them
+    records.map(serialize[TestRecord])
 
-   // then: we should make it to then end
-   true
- }
+    // then: we should make it to then end
+    true
+  }
 
- behavior of "ThriftDeserializable[T]"
+  behavior of "ThriftDeserializable[T]"
 
- it should "be able to create a `Deserializable[TestRecord]`" in {
-   // given: a serialized `TestRecord`
-   val bytes = serialize(TestRecord("", 0l))
+  it should "be able to create a `Deserializable[TestRecord]`" in {
+    // given: a serialized `TestRecord`
+    val bytes = serialize(TestRecord("", 0l))
 
-   // when: we provide a `ThriftDeserializable[TestRecord]`
-   implicit object DeserializableTestRecord
-     extends ThriftDeserializable[TestRecord]
+    // when: we provide a `ThriftDeserializable[TestRecord]`
+    implicit object DeserializableTestRecord
+      extends ThriftDeserializable[TestRecord]
 
-   // then: we should be able to deserialize `TestRecord`s
-   deserialize(bytes)
- }
+    // then: we should be able to deserialize `TestRecord`s
+    deserialize(bytes)
+  }
 
- it should "be reversible" in {
-   // given: a `TestRecord` and a `ThriftDeserializable[TestRecord]`
-   val record = TestRecord("", 0l)
-   implicit object DeserializableTestRecord
-     extends ThriftDeserializable[TestRecord]
+  it should "be reversible" in {
+    // given: a `TestRecord` and a `ThriftDeserializable[TestRecord]`
+    val record = TestRecord("", 0l)
+    implicit object DeserializableTestRecord
+      extends ThriftDeserializable[TestRecord]
 
-   // when: we deserialize a serialized record
-   val result = deserialize(serialize(record))
+    // when: we deserialize a serialized record
+    val result = deserialize(serialize(record))
 
-   // then: the record should survive the round trip
-   result.get should be(record)
- }
+    // then: the record should survive the round trip
+    result.get should be(record)
+  }
 
- it should "return `None` when given bad input" in {
-   // given: some bad bytes
-   val badBytes = "baaad".toCharArray.map(_.toByte)
-   implicit object DeserializableTestRecord
-     extends ThriftDeserializable[TestRecord]
+  it should "return `None` when given bad input" in {
+    // given: some bad bytes
+    val badBytes = "baaad".toCharArray.map(_.toByte)
+    implicit object DeserializableTestRecord
+      extends ThriftDeserializable[TestRecord]
 
-   // when: we serialize them
-   val result = deserialize(badBytes)
+    // when: we serialize them
+    val result = deserialize(badBytes)
 
-   // then: the result should be `None`
-   result should be(None)
- }
+    // then: the result should be `None`
+    result should be(None)
+  }
 
- behavior of "makeThriftDeserializer"
+  behavior of "makeThriftDeserializer"
 
- it should "provide an alternative API for deserializing Thrift" in {
-   // given: a `TestRecord`
-   val record = TestRecord("", 0l)
+  it should "provide an alternative API for deserializing Thrift" in {
+    // given: a `TestRecord`
+    val record = TestRecord("", 0l)
 
-   // when: we invoke `makeAvroDeserializer`
-   val deserializeTestRecord =
-     makeThriftDeserializer[TestRecord]
+    // when: we invoke `makeAvroDeserializer`
+    val deserializeTestRecord =
+      makeThriftDeserializer[TestRecord]
 
-   // then: the resulting deserializer should behave as expected
-   deserializeTestRecord(serialize(record)).get should be(record)
- }
+    // then: the resulting deserializer should behave as expected
+    deserializeTestRecord(serialize(record)).get should be(record)
+  }
 }
