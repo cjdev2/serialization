09314079a1f0b9580b878cdb75a5c05651366e70
Daniel Brice <dbrice@cj.com>
2017-01-04 14:56:32 -0800

    starting work on thrift

---
+++ src/main/scala/com/cj/serialization/thrift/package.scala
@@ -0,0 +1,27 @@
+package com.cj.serialization
+
+import com.twitter.scrooge.ThriftStruct
+
+package object thrift {
+
+ implicit object SerializableThriftStruct extends Serializable[ThriftStruct] {
+   def serialize(t: ThriftStruct): Array[Byte] = {
+     ???
+     Array()
+   }
+ }
+
+ class ThriftDeserializable[T <: ThriftStruct] extends Deserializable[T] {
+   def deserialize(bytes: Array[Byte]): Option[T] = {
+     ???
+     None
+   }
+ }
+
+ def makeThriftDeserializer[T <: ThriftStruct]: Array[Byte] => Option[T] = {
+
+   implicit object DeserializableStruct extends ThriftDeserializable[T]
+
+   bytes => deserialize(bytes)
+ }
+}
