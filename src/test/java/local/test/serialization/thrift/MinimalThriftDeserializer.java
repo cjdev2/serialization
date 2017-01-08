9dcc80be26b9cdfaf6a5a60b9a66c18c474441c9
Daniel Brice <dbrice@cj.com>
2017-01-07 23:45:44 -0800

    need these for testing

--- src/test/java/local/test/serialization/thrift/MinimalThriftDeserializer.java
+++ src/test/java/local/test/serialization/thrift/MinimalThriftDeserializer.java
@@ -1,10 +1,13 @@
 package local.test.serialization.thrift;
 
 import local.test.serialization.thrift.java.TestRecord;
+import org.apache.thrift.TDeserializer;
 
 public class MinimalThriftDeserializer {
 
-    public static TestRecord deserialize(byte[] bytes) {
-        return null;
+    public static TestRecord deserialize(byte[] bytes) throws org.apache.thrift.TException {
+        TestRecord record = new TestRecord();
+        new TDeserializer().deserialize(record, bytes);
+        return record;
     }
 }
--- src/test/java/local/test/serialization/thrift/MinimalThriftSerializer.java
+++ src/test/java/local/test/serialization/thrift/MinimalThriftSerializer.java
@@ -1,10 +1,11 @@
 package local.test.serialization.thrift;
 
 import local.test.serialization.thrift.java.TestRecord;
+import org.apache.thrift.TSerializer;
 
 public class MinimalThriftSerializer {
 
-    public static byte[] serialize(TestRecord t) {
-        return null;
+    public static byte[] serialize(TestRecord t) throws org.apache.thrift.TException {
+        return new TSerializer().serialize(t);
     }
 }
